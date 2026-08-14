fb3841fba9108a47da934f3f85c30efa55d675fe
Eduards Priednieks <eduards.priednieks@gmail.com>
2026-08-15 10:17:31 +1200

    w3 a2 v2

--- week3/activity2/main.py
+++ week3/activity2/main.py
@@ -2,14 +2,18 @@ from rich import print
 from ucimlrepo import fetch_ucirepo 
 
 def main():
-  data = open("junk.txt")
-  lines = data.readlines()
-  data.close()  
-  print("Total lines in file: "+str(len(lines)))
+  file = open("junk.txt")
+  lines = file.readlines()
+  file.close()  
+  print("[green]Total lines in file: [bold]"+str(len(lines))+"[/bold][/green]")
 
   file=open("junk.txt", "a")
   file.write("text file nanalyssis\n")
   file.close()
+
+  file = open("junk.txt")
+  lines = file.readlines()
+  file.close()  
 
   file=open("junk.txt", "w")
   for l in lines:
